ddd73779a78a9a5e679826f331ac9c9508149b93
Sikandar Chauhan <sikandarc989@gmail.com>
2024-11-21 22:14:20 +0530

    add Computer to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,72 +1,40 @@
 import { styles } from "../global";
 import { motion } from "framer-motion";
-import { linkedin, githubs, instagram } from "../assets";
+import Typewriter from "typewriter-effect";
+import ComputersCanvas from "../canvas/Computers";
 
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`absolute inset-0 top-[50px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-center justify-center gap-5 `}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915EFF]">Sikandar Chauhan</span>
           </h1>
-
-          <h1
-            className={`font-black text-white lg:text-[60px] xs:text-[50px] text-[20px] lg:leading-[80px]`}
-          >
-            Web Developer
-          </h1>
-
-          <p className="md:w-1/2 mt-2 text-center text-xs md:text-sm lg:text-md">
-            A passionate frontend focused web developer specializes in creating
-            the visual elements of websites, ensuring an intuitive and engaging
-            user experience.
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Full Stack Developer
+            <Typewriter
+              options={{
+                strings: ["Web App Developer", "MERN Stack", "MEAN Stack"],
+                autoStart: true,
+                loop: true,
+                loopCount: Infinity,
+                deleteSpeed: "natural",
+                pauseFor: 1000,
+              }}
+            />
           </p>
-
-          <div className={`flex my-6 items-center gap-8`}>
-            {/* <a
-              href="https://robinmahto.hashnode.dev/"
-              target="blank"
-              title="hashnode"
-              className="bg-[#915EFF] ease-in duration-200 hover:bg-tertiary p-2 rounded-full"
-            >
-              <img src={hashnode} alt="hashnode" />
-            </a> */}
-            <a
-              href="https://www.linkedin.com/in/sikandar-chauhan/"
-              target="blank"
-              title="linkedin"
-              className="bg-[#915EFF] ease-in duration-200 hover:bg-tertiary p-2 rounded-full"
-            >
-              <img
-                src={linkedin}
-                alt="linkedin"
-                className="cursor-pointer text-white"
-              />
-            </a>
-            <a
-              href="https://github.com/thisissikandar"
-              target="blank"
-              title="github"
-              className="bg-[#915EFF] ease-in duration-200 hover:bg-tertiary  p-2 rounded-full"
-            >
-              <img src={githubs} alt="githubs" />
-            </a>
-            <a
-              href="https://www.instagram.com/thisissikandar/"
-              target="blank"
-              title="instagram"
-              className="bg-[#915EFF] ease-in duration-200 hover:bg-tertiary p-2 rounded-full"
-            >
-              <img src={instagram} alt="instagram" />
-            </a>
-          </div>
         </div>
       </div>
-
-      <div className="absolute xs:bottom-10 bottom-16  w-full flex justify-center items-center">
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,17 +3,16 @@ import { styles } from "../global";
 import { staggerContainer } from "../utils/motion";
 
 const StarWrapper = (Component, idName) =>
-
   function HOC() {
     return (
       <motion.section
         variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
-        <span className='hash-span' id={idName}>
+        <span className="hash-span" id={idName}>
           &nbsp;
         </span>
 
